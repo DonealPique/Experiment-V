0420ee75a7a8a9cddf4c21267da3acea12391ac6
Doneal Piqué <135713732+DonealPique@users.noreply.github.com>
2024-06-11 09:42:23 +0200

    fix

---
+++ Install.lua
@@ -0,0 +1,22 @@
+local files = {
+    "elavator.lua",
+    "keycardsys.lua",
+    "levercontroller.lua",
+    "mob_grinder.lua",
+    "RaphaelOS.lua",
+    "TextOnMonitor.lua"
+}
+
+
+local baseURL = "https://raw.githubusercontent.com/DonealPique/Experiment-V/main/scripts/"
+local worldURL = "https://drive.google.com/file/d/1QTTchrM8epDm188MYBwzM2LN87Z9Ez4p/view?usp=sharing"
+
+for _, file in ipairs(files) do
+    local url = baseURL .. file
+    shell.run("wget", url, file)
+end
+
+shell.run("wget", worldURL, "Experiment V.zip")
+
+print("All scripts and world have been downloaded.")
+print("Please restart the computer to apply changes. (Ctrl + R + Enter)")
