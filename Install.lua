84e5af9e3d67ad9224acfdfa5b5193c74b8e89a0
Doneal Piqué <135713732+DonealPique@users.noreply.github.com>
2024-06-11 09:58:19 +0200

    Update Install.lua

--- Install.lua
+++ Install.lua
@@ -9,14 +9,14 @@ local files = {
 
 
 local baseURL = "https://raw.githubusercontent.com/DonealPique/Experiment-V/main/scripts/"
-local worldURL = "https://drive.google.com/file/d/1QTTchrM8epDm188MYBwzM2LN87Z9Ez4p/view?usp=sharing"
+local worldURL = "https://drive.google.com/file/d/1XyMd0RAp9B-t8matrl0L1Wfnak1xhzHc/view?usp=sharing"
 
 for _, file in ipairs(files) do
     local url = baseURL .. file
     shell.run("wget", url, file)
 end
 
-shell.run("wget", worldURL, "Experiment V.zip")
+shell.run("wget", worldURL, "Experiment-V.zip")
 
 print("All scripts and world have been downloaded.")
 print("Please restart the computer to apply changes. (Ctrl + R + Enter)")
